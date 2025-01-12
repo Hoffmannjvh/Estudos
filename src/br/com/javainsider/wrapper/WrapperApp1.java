063794e3eed777a4de23d43ccb708317ca539b53
Hoffmann <contato.jhonnybjj@gmail.com>
2025-01-12 12:36:54 -0300

    Estudos sobre Wrapper

---
+++ src/br/com/javainsider/wrapper/WrapperApp1.java
@@ -0,0 +1,14 @@
+package br.com.javainsider.wrapper;
+
+public class WrapperApp1 {
+
+    public static void main(String[] args) {
+
+        int i = 10;
+        Integer x = Integer.valueOf(i);
+
+        System.out.println(x);
+
+    }
+
+}
