0a73ce5d81f2e948ed4ab66845991410ddf48483
João vitor <contato.jhonnybjj@gmail.com>
2025-02-13 16:30:16 -0300

    RandomMatchs

---
+++ src/br/com/javainsider/other/ramdom/RandomApp1.java
@@ -0,0 +1,16 @@
+package br.com.javainsider.other.ramdom;
+
+
+public class RandomApp1 {
+
+    public static void main(String[] args) {
+
+        for(int i = 0; i < 10; i++) {
+            double d = Math.random();
+            System.out.println(d);
+        }
+
+
+    }
+
+}
