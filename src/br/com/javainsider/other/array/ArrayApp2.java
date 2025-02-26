ad80742ad5defab20a62c049a13bfa1996469233
Hoffmann <contato.jhonnybjj@gmail.com>
2025-02-26 00:29:49 -0300

    arrays

---
+++ src/br/com/javainsider/other/array/ArrayApp2.java
@@ -0,0 +1,17 @@
+package br.com.javainsider.other.array;
+
+import java.util.Arrays;
+
+public class ArrayApp2 {
+
+    public static void main(String[] args) {
+
+        String[] array = new String[2];
+        array[0] = "A";
+        array[1] = "B";
+
+        System.out.println(Arrays.toString(array));
+
+    }
+
+}
