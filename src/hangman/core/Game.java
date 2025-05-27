5b676a2bd4016d02df1130c095355d6e9a378c61
João vitor <contato.jhonnybjj@gmail.com>
2025-05-27 19:37:05 -0300

    Criando o loop do jogo

---
+++ src/hangman/core/Game.java
@@ -0,0 +1,20 @@
+package hangman.core;
+
+import hangman.io.Input;
+import hangman.io.Output;
+
+public class Game {
+    public void start() {
+
+        Dictionary dictionary = Dictionary.instance();
+        Word word = dictionary.nextWord();
+        Output.writeToConsole(word);
+
+        while (true) {
+            String text = Input.readFromKeyboard("Letter");
+            word.reveal(text.charAt(0));
+
+            Output.writeToConsole(word);
+        }
+    }
+}
--- src/hangman/test/Test.java
+++ src/hangman/test/Test.java
@@ -1,4 +1,6 @@
-package hangman.core;
+package hangman.test;
+
+import hangman.core.Dictionary;
 
 public class Test {
 
