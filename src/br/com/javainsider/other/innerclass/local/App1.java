a7986826c577b84eee2d74ab4d21700fb07bce5c
Hoffmann <contato.jhonnybjj@gmail.com>
2025-06-23 23:37:49 -0300

    Estudos sobre local inner class

---
+++ src/br/com/javainsider/other/innerclass/local/App1.java
@@ -0,0 +1,10 @@
+package br.com.javainsider.other.innerclass.local;
+
+public class App1 {
+
+    public static void main(String[] args) {
+        Application application = new Application();
+        application.greet();
+    }
+
+}
---
+++ src/br/com/javainsider/other/innerclass/local/App2.java
@@ -0,0 +1,10 @@
+package br.com.javainsider.other.innerclass.local;
+
+public class App2 {
+
+    public static void main(String[] args) {
+        Application2 application = new Application2();
+        application.greet("Pedro");
+    }
+
+}
